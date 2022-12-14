a0a9a0cce50fd792209859b94a84657c71d9f6ff
Fouad Mahmoud <>
2022-12-14 09:09:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#greeting for ai project
+print("Welcom to Band Name Generator.")
+city = input("what's the name of the city you grow up in?")
+pet = input("what's your pet's name?")
+print("your band name could be "+city+" "+pet)
